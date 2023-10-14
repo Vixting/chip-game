561f2545348e5d425d1786e49a46499b35c6df6f
Vixting <59802277+Vixting@users.noreply.github.com>
2023-10-14 16:42:49 +0100

    Added wall

--- src/main/java/com/group4/chipgame/LevelLoader.java
+++ src/main/java/com/group4/chipgame/LevelLoader.java
@@ -1,19 +1,17 @@
 package com.group4.chipgame;
 
-import com.group4.chipgame.tiles.Tile;
-import com.group4.chipgame.tiles.Path;
-import com.group4.chipgame.tiles.Water;
-import com.group4.chipgame.actors.Actor;
-import com.group4.chipgame.actors.Player;
-import com.group4.chipgame.actors.Creeper;
-
+import com.group4.chipgame.actors.*;
+import com.group4.chipgame.tiles.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 public class LevelLoader {
     private JSONObject loadJsonFromResource(String path) throws IOException {
@@ -21,10 +19,23 @@ public class LevelLoader {
             if (resourceStream == null) {
                 throw new IOException("Resource not found: " + path);
             }
-            String content = new String(resourceStream.readAllBytes());
-            return new JSONObject(content);
+            return new JSONObject(new String(resourceStream.readAllBytes()));
         }
     }
+
+    private final Map<String, BiFunction<Integer, Integer, Tile>> tileCreators = new HashMap<>() {{
+        put("P", (x, y) -> new Path());
+        put("W", (x, y) -> new Water());
+        put("I", (x, y) -> new Wall());
+        // Add more tiles as needed
+    }};
+
+    private final Map<String, BiFunction<Integer, Integer, Actor>> actorCreators = new HashMap<>() {{
+        put("Player", Player::new);
+        put("Creeper", Creeper::new);
+        put("MovableBlock", MovableBlock::new);
+        // Add more actors as needed
+    }};
 
     public Tile[][] loadTiles(String levelFilePath) throws IOException {
         JSONObject levelData = loadJsonFromResource(levelFilePath);
@@ -39,11 +50,7 @@ public class LevelLoader {
             JSONArray row = tilesArray.getJSONArray(y);
             for (int x = 0; x < width; x++) {
                 String tileType = row.getString(x);
-                switch (tileType) {
-                    case "P" -> levelTiles[y][x] = new Path();
-                    case "W" -> levelTiles[y][x] = new Water();
-                    // Add more cases as needed
-                }
+                levelTiles[y][x] = tileCreators.getOrDefault(tileType, (a, b) -> null).apply(x, y);
             }
         }
         return levelTiles;
@@ -61,13 +68,7 @@ public class LevelLoader {
             int x = actorData.getInt("x");
             int y = actorData.getInt("y");
 
-            if ("Player".equals(type)) {
-                System.out.println("Player found at " + x + ", " + y);
-                actors.add(new Player(x, y));
-            } else if ("Creeper".equals(type)) {
-                actors.add(new Creeper(x, y));
-
-            }
+            actors.add(actorCreators.getOrDefault(type, (a, b) -> null).apply(x, y));
         }
 
         return actors;
