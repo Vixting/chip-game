67640ccbe0211868a78f709a60939c7f6bd81d2e
Vixting <59802277+Vixting@users.noreply.github.com>
2023-10-15 07:18:17 +0100

    Camera cleanup, comments, fixed alignment

--- src/main/java/com/group4/chipgame/Camera.java
+++ src/main/java/com/group4/chipgame/Camera.java
@@ -7,35 +7,70 @@ import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class Camera {
+    // The main game pane that the camera will be applied to.
     private final Pane gamePane;
-    private  Rectangle2D viewPort;
-    private Actor target; // The actor the camera should follow.
 
-    // Constructor for Camera
+    // The viewport defines the visible area of the gamePane.
+    private Rectangle2D viewPort;
+
+    // The actor (e.g., player character) that the camera will follow.
+    private Actor target;
+
+    // Timer used to periodically adjust the camera's position.
+    private AnimationTimer timer;
+
+    /**
+     * Constructor for the Camera class.
+     *
+     * @param gamePane The main game pane.
+     * @param viewWidth Width of the viewport.
+     * @param viewHeight Height of the viewport.
+     */
     public Camera(Pane gamePane, double viewWidth, double viewHeight) {
         this.gamePane = gamePane;
         this.viewPort = new Rectangle2D(0, 0, viewWidth, viewHeight);
 
-        // Clipping to ensure only a portion of the gamePane is visible.
+        // Set a clip to ensure only a portion of the gamePane is visible.
         this.gamePane.setClip(new javafx.scene.shape.Rectangle(viewWidth, viewHeight));
 
-        // Add listeners to the width and height properties of the gamePane
-        gamePane.widthProperty().addListener((observable, oldValue, newValue) -> {
-            viewPort = new Rectangle2D(viewPort.getMinX(), viewPort.getMinY(), newValue.doubleValue(), viewPort.getHeight());
-            adjustCamera();
-        });
-        gamePane.heightProperty().addListener((observable, oldValue, newValue) -> {
-            viewPort = new Rectangle2D(viewPort.getMinX(), viewPort.getMinY(), viewPort.getWidth(), newValue.doubleValue());
-            adjustCamera();
-        });
+        // Add listeners to handle changes in the gamePane's size.
+        addPaneSizeListeners();
     }
 
-    // Set the target actor for the camera to follow
+    /**
+     * Set the target actor for the camera to follow.
+     *
+     * @param target The actor to be followed by the camera.
+     */
     public void setTarget(Actor target) {
         this.target = target;
+        startCameraAdjustmentTimer();
+    }
 
-        // Create an AnimationTimer to periodically adjust the camera
-        AnimationTimer timer = new AnimationTimer() {
+    // Add listeners to the gamePane's width and height properties.
+    private void addPaneSizeListeners() {
+        gamePane.widthProperty().addListener((observable, oldValue, newValue) -> updateViewPortWidth(newValue.doubleValue()));
+        gamePane.heightProperty().addListener((observable, oldValue, newValue) -> updateViewPortHeight(newValue.doubleValue()));
+    }
+
+    // Update the viewport's width and adjust the camera accordingly.
+    private void updateViewPortWidth(double newWidth) {
+        viewPort = new Rectangle2D(viewPort.getMinX(), viewPort.getMinY(), newWidth, viewPort.getHeight());
+        adjustCamera();
+    }
+
+    // Update the viewport's height and adjust the camera accordingly.
+    private void updateViewPortHeight(double newHeight) {
+        viewPort = new Rectangle2D(viewPort.getMinX(), viewPort.getMinY(), viewPort.getWidth(), newHeight);
+        adjustCamera();
+    }
+
+    // Start a timer to periodically adjust the camera's position.
+    private void startCameraAdjustmentTimer() {
+        if (timer != null) {
+            timer.stop();
+        }
+        timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 adjustCamera();
@@ -44,60 +79,58 @@ public class Camera {
         timer.start();
     }
 
-    // Method to adjust the camera's position
-    void adjustCamera() {
+    // Adjust the camera's position based on the target actor's position.
+    private void adjustCamera() {
         if (target == null) {
             return;
         }
 
-        // Get the current scaling factors for the gamePane
-        double scaleX = gamePane.getScaleX();
-        double scaleY = gamePane.getScaleY();
-
-        // Calculate the center coordinates of the target actor
+        // Calculate the center coordinates of the target actor.
         double targetCenterX = target.getLayoutX() + target.getFitWidth() / 2.0;
         double targetCenterY = target.getLayoutY() + target.getFitHeight() / 2.0;
 
-        // Calculate the new translation values for the camera
+        // Calculate the new translation values for the camera.
+        double scaleX = gamePane.getScaleX();
+        double scaleY = gamePane.getScaleY();
         double newTranslateX = (viewPort.getWidth() / 2.0 - targetCenterX) * scaleX;
         double newTranslateY = (viewPort.getHeight() / 2.0 - targetCenterY) * scaleY;
 
-        // Define the minimum and maximum translation values to avoid going out of bounds
-        double minX = 0;
-        double minY = 0;
-        double maxX = (viewPort.getWidth() - viewPort.getWidth() / scaleX);
-        double maxY = (viewPort.getHeight() - viewPort.getHeight() / scaleY);
+        // Clamp the translation values to ensure the camera stays within bounds.
+        newTranslateX = -clampTranslate(-newTranslateX, viewPort.getWidth(), viewPort.getWidth() / scaleX);
+        newTranslateY = -clampTranslate(-newTranslateY, viewPort.getHeight(), viewPort.getHeight() / scaleY);
 
-        // Clamp the translation values to stay within bounds
-        newTranslateX = -clampTranslate(-newTranslateX, minX, maxX);
-        newTranslateY = -clampTranslate(-newTranslateY, minY, maxY);
+        // Animate the camera's translation to the new position.
+        animateCameraTranslation(newTranslateX, newTranslateY);
+    }
 
-        // Unbind previous translations and create a Timeline to smoothly update the camera position
+    // Animate the camera's translation to smoothly move to the specified position.
+    private void animateCameraTranslation(double newTranslateX, double newTranslateY) {
         gamePane.translateXProperty().unbind();
         gamePane.translateYProperty().unbind();
 
         Timeline timeline = new Timeline();
-
-        // Define keyframes to animate the camera's translation
         KeyValue kvX = new KeyValue(gamePane.translateXProperty(), newTranslateX);
         KeyValue kvY = new KeyValue(gamePane.translateYProperty(), newTranslateY);
         KeyFrame kf = new KeyFrame(Duration.millis(1000), kvX, kvY);
-
-        // Add the keyframe to the timeline and play the animation
         timeline.getKeyFrames().add(kf);
         timeline.play();
     }
 
-    // Helper method to clamp a value within a specified range.
+    /**
+     * Helper method to clamp a value within a specified range.
+     *
+     * @param value The value to be clamped.
+     * @param paneSize The size of the game pane.
+     * @param viewportSize The size of the viewport.
+     * @return The clamped value.
+     */
     private double clampTranslate(double value, double paneSize, double viewportSize) {
-        double scale = gamePane.getScaleX();  // Assuming x and y scales are the same.
+        double scale = gamePane.getScaleX();
         double minTranslate = paneSize * scale - viewportSize;
 
         if (minTranslate < 0) {
-            // When viewport is larger than pane, clamp between 0 and minTranslate
             return Math.min(0, Math.max(value, minTranslate));
         } else {
-            // When pane is larger than or equal to viewport, no clamping needed
             return value;
         }
     }
