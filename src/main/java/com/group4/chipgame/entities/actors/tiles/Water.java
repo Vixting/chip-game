1d8f3b780c4d42b4d505e1e3cd567a61f707e549
Vixting <59802277+Vixting@users.noreply.github.com>
2023-12-10 22:42:36 +0000

    Fixed kills player.

--- src/main/java/com/group4/chipgame/entities/actors/tiles/Water.java
+++ src/main/java/com/group4/chipgame/entities/actors/tiles/Water.java
@@ -3,13 +3,16 @@ package com.group4.chipgame.entities.actors.tiles;
 import com.group4.chipgame.Direction;
 import com.group4.chipgame.Level.LevelRenderer;
 import com.group4.chipgame.entities.actors.Actor;
+import com.group4.chipgame.entities.actors.Player;
 
 /**
  * Represents a water tile in the ChipGame.
  * Water tiles are non-walkable obstacles in the game level.
+ * @author William Buckley
  */
 public class Water extends Tile {
-    private static final String WATER_IMAGE_PATH = "/images/chipgame/tiles/water.jpg";
+    private static final String WATER_IMAGE_PATH =
+            "/images/chipgame/tiles/water.jpg";
 
     /**
      * Constructs a new Water tile.
@@ -25,7 +28,7 @@ public class Water extends Tile {
      */
     @Override
     public boolean isWalkable() {
-        return false;
+        return true;
     }
 
     /**
@@ -34,10 +37,35 @@ public class Water extends Tile {
      *
      * @param actor The actor stepping on the tile.
      * @param levelRenderer The renderer for the level.
-     * @param incomingDirection The direction from which the actor steps onto the tile.
+     * @param incomingDirection The direction
+     *                          from which the actor steps onto the tile.
      */
     @Override
-    public void onStep(Actor actor, LevelRenderer levelRenderer, Direction incomingDirection) {
-        // No specific action defined for stepping on a Water tile.
+    public void onStep(final Actor actor,
+                       final LevelRenderer levelRenderer,
+                       final Direction incomingDirection) {
+        killPlayerAt(actor.getPosition().getX(),
+                actor.getPosition().getY(),
+                levelRenderer);
+    }
+
+    /**
+     * Kills the player at the specified position.
+     * This is called when a block is pushed onto a player's position.
+     *
+     * @param x             The x-coordinate where the player is located.
+     * @param y             The y-coordinate where the player is located.
+     * @param levelRenderer The renderer for the game level.
+     */
+    private void killPlayerAt(final double x,
+                              final double y,
+                              final LevelRenderer levelRenderer) {
+        levelRenderer.getActors().stream()
+                .filter(actor
+                        -> actor instanceof Player
+                        && actor.getPosition().getX() == x
+                        && actor.getPosition().getY() == y)
+                .findFirst()
+                .ifPresent(player -> ((Player) player).kill(levelRenderer));
     }
 }
